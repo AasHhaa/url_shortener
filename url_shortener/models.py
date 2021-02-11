3b3c3af7d6edc3dcbc471feb0111b9c77a4ba4c5
As_ha <ashahaasa@gmail.com>
2021-02-11 13:16:52 +0200

    Removing unnecessary files, and adding a link lifetime.

--- url_shortener/models.py
+++ url_shortener/models.py
@@ -8,8 +8,8 @@ class Link(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     original_url = db.Column(db.String(512))
     short_url = db.Column(db.String(3), unique=True)
-    visits = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.now())
+    short_url_lifetime = db.Column(db.DateTime)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
--- url_shortener/routers.py
+++ url_shortener/routers.py
@@ -1,13 +1,19 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect
 from .models import Link
 from .extensions import db
+from datetime import datetime, timedelta
 
 short = Blueprint('short', __name__)
 
 
 @short.route('/<short_url>')
 def redirect_to_url(short_url):
-    pass
+    link = Link.query.filter_by(short_url=short_url).first_or_404()
+    if link.short_url_lifetime < datetime.now():
+        db.session.delete(link)
+        db.session.commit()
+        return "Your link is expired", 404
+    return redirect(link.original_url)
 
 
 @short.route('/')
@@ -18,17 +24,20 @@ def index():
 @short.route('/add_link', methods=['POST'])
 def add_link():
     original_url = request.form['original_url']
-    link = Link(original_url=original_url)
+    lifetime = int(request.form['lifetime']) if request.form['lifetime'] else None
+    if lifetime:
+        if 1 <= lifetime <= 365:
+            lifetime = datetime.now() + timedelta(lifetime)
+        else:
+            return "Lifetime is not in allowed range",
+    else:
+        lifetime = datetime.now() + timedelta(90)
+    link = Link(original_url=original_url, short_url_lifetime=lifetime)
     db.session.add(link)
     db.session.commit()
     return render_template('link_added.html',
                            new_link=link.short_url,
                            original_url=link.original_url)
-
-
-@short.route('/stats')
-def stats():
-    pass
 
 
 @short.errorhandler(404)
